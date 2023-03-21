e98b85aa7079f40b29b22d79b61641fdb025128c
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-21 22:20:34 +0700

    Add routes and controller for users with mysql

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const authorization=require('./middleware/authorization');
 const expense=require('./controller/expense');
+const user=require('./controller/user');
 
 // - Get expense list
 router.get('/expense/list', authorization.authorize, expense.list);
@@ -10,19 +11,23 @@ router.get('/expense/list', authorization.authorize, expense.list);
 router.get('/expense/details', authorization.authorize, expense.details);
 
 // - Create new expense data
-router.post('/expense/create', authorization.authorize, expense.create);
+router.post('/expense/details', authorization.authorize, expense.create);
 
 // - Edit expense data
-router.patch('/expense/edit', authorization.authorize, expense.edit);
+router.patch('/expense/details', authorization.authorize, expense.edit);
 
 // - Delete expense data
-router.delete('/expense/delete', authorization.authorize, expense.delete);
+router.delete('/expense/details', authorization.authorize, expense.delete);
 
 // - Get total expense by date range
 router.get('/expense/dates', authorization.authorize, expense.byDateRange);
 
 // - Get total expense by category
 router.get('/expense/categories', authorization.authorize, expense.byCategory);
+
+
+router.get('/user/list', authorization.authorize, user.list);
+router.post('/user/list', authorization.authorize, user.list);
 
 
 module.exports=router;
